afb27907216dd3447b9a628d115ccac26e3c36b0
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-05-28 15:22:40 -0300

    working on the form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,50 @@
-
+import {useState} from 'react'
 
 const Formulario = () => {
+
+  const [busqueda, setBusqueda] = useState({
+    artista: '',
+    cancion: ''
+  })
+
   return (
-    <div>
-      Formulario
-    </div>
-  )
+    <form>
+      <legend>Busca por Artistas y Canción</legend>
+      <div className="form-grid">
+        <div>
+          <label>Artista</label>
+          <input
+            type="text"
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label>Canción</label>
+          <input
+            type="text"
+            name="cancion"
+            placeholder="Nombre Canción"
+            value={busqueda.cancion}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
+  );
 }
 
 export default Formulario
