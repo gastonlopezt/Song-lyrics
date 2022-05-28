80b938de5e50dddd9fdbe957af2ed4ee4de4952d
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-05-28 15:48:17 -0300

    form + context

--- src/components/AppLetras.jsx
+++ src/components/AppLetras.jsx
@@ -1,12 +1,17 @@
 import Formulario from './Formulario'
+import Alerta from './Alerta'
+import useLetras from '../hooks/useLetras'
 
 const AppLetras = () => {
+
+  const { alerta } = useLetras()
+
   return (
     <>
       <header>Búsqueda de letras de canciones</header>
       <Formulario />
       <main>
-
+        {alerta && <Alerta>{alerta}</Alerta>}
       </main>
     </>
   )
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,29 @@
 import {useState} from 'react'
+import useLetras from '../hooks/useLetras'
 
 const Formulario = () => {
+
+  const {setAlerta} = useLetras()
 
   const [busqueda, setBusqueda] = useState({
     artista: '',
     cancion: ''
   })
 
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(Object.values(busqueda).includes('')){
+      setAlerta('Coloca nombre de artista y Canción')
+      return
+    }
+    setAlerta('')
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit}
+    >
       <legend>Busca por Artistas y Canción</legend>
       <div className="form-grid">
         <div>
