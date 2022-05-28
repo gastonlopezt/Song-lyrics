c24022e8a0d88887ac4fd1ca3bf82d48951bc04e
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-05-28 15:12:14 -0300

    modeling

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,15 @@
+import Formulario from './Formulario'
+
+const AppLetras = () => {
+  return (
+    <>
+      <header>Búsqueda de letras de canciones</header>
+      <Formulario />
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default AppLetras
